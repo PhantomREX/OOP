e74dd8df7e5f98108a0ded6c6c8061e158762517
Yasin <43208325+PhantomREX@users.noreply.github.com>
2022-04-08 22:37:21 +0300

    Add files via upload

---
+++ Student_OOP/Student_OOP/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student s = new Student();
+            s.Name = "Yasin";
+            s.Surname = "TOP";
+            s.No = 453;
+            s.Class = "Laboratuvar 3";
+
+            Console.WriteLine("Öğrenci Bilgileri");
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Adı: " + s.Name);
+            Console.WriteLine("Soyadı: " + s.Surname);
+            Console.WriteLine("No: " + s.No);
+            Console.WriteLine("Sınıfı: " + s.Class);
+
+
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
---
+++ Student_OOP/Student_OOP/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_OOP
+{
+    internal class Student
+    {
+
+
+        public string Name, Surname, Class;
+
+        public int No;
+
+
+
+
+    }
+}
